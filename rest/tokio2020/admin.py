62f1194da456808a553115b85325553156c120d5
Juan Martínez <juan.c.a.martinez@accenture.com>
2020-01-30 17:56:51 -0500

    Modelos nuevos

--- rest/tokio2020/admin.py
+++ rest/tokio2020/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
-from .models import CustomUser, Athletes
+from .models import CustomUser, Athlete, Sport, Mode, Event
 
 admin.site.register(CustomUser)
-admin.site.register(Athletes)
+admin.site.register(Athlete)
+admin.site.register(Sport)
+admin.site.register(Mode)
+admin.site.register(Event)
+
+
+class EventAdmin(admin.ModelAdmin):
+    exclude = ['datetime', ]
--- rest/tokio2020/models.py
+++ rest/tokio2020/models.py
@@ -10,13 +10,14 @@ class CustomUser(AbstractUser):
     last_name = models.CharField(max_length=50, verbose_name="Apellidos")
 
 
-class Athletes(models.Model):
+class Athlete(models.Model):
     """
     Deportistas
     """
     first_name = models.CharField(max_length=50, verbose_name="Nombres")
     last_name = models.CharField(max_length=50, verbose_name="Apellidos")
-    image = models.ImageField(verbose_name="Foto")
+    image = models.ImageField(null=True, blank=True,
+                              upload_to="files/athletes", verbose_name="Foto")
     birthday = models.DateField(verbose_name="Fecha de Nacimiento")
     birthplace = models.CharField(
         max_length=50, verbose_name="Lugar de nacimiento")
@@ -26,3 +27,58 @@ class Athletes(models.Model):
     height = models.DecimalField(
         max_digits=3, decimal_places=2, max_length=50, verbose_name="Altura", help_text="metros")
     coach = models.CharField(max_length=100, verbose_name="Entrenador")
+
+    def __str__(self):
+        return '%s %s' % (self.first_name, self.last_name)
+
+
+class Sport(models.Model):
+    """
+    Deporte
+    """
+    name = models.CharField(max_length=50, verbose_name="Nombre")
+    icono = models.FileField(
+        null=True, blank=True, upload_to="files/icons", verbose_name="Ícono")
+
+    def __str__(self):
+        return self.name
+
+
+class Mode(models.Model):
+    """
+    Modalidad de deporte
+    """
+    Gender = (
+        ('M', 'Masculino'),
+        ('F', 'Femenino'),
+    )
+
+    name = models.CharField(max_length=50, verbose_name="Nombre")
+    gender = models.CharField(
+        max_length=50, verbose_name="Nombre", choices=Gender, default='M')
+    sport_id = models.ForeignKey(
+        Sport, on_delete=models.CASCADE, related_name='sport_mode', verbose_name="Deporte")
+
+    def __str__(self):
+        return self.name
+
+
+class Event(models.Model):
+    """
+    Modalidad de deporte
+    """
+    date = models.DateField(verbose_name="Fecha")
+    time = models.TimeField(verbose_name="Hora")
+    datetime = models.DateTimeField(verbose_name="Fecha y Hora", null=True)
+    sport_id = models.ForeignKey(
+        Sport, on_delete=models.CASCADE, related_name='sport_event', verbose_name="Deporte")
+    mode_id = models.ForeignKey(
+        Mode, on_delete=models.CASCADE, related_name='modet_event', verbose_name="Modalidad")
+    result = models.CharField(max_length=50, verbose_name="Resultado")
+
+    def save(self, *args, **kwargs):
+        self.datetime = '%s %s' % (self.date, self.time)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return '%s %s' % (self.datetime, self.sport_id)
