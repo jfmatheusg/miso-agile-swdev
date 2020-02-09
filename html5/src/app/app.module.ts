86a521972b0c6cabf9c269fec7ff3a9d50586b7c
Sergio David Romero Maldonado <sromerom@psl.com.co>
2020-02-08 21:37:37 -0500

    Importo módulo Bootstrap en modulo principal

--- html5/src/app/app.module.ts
+++ html5/src/app/app.module.ts
@@ -1,20 +1,18 @@
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DemoMaterialModule } from './material.module';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { DemoMaterialModule } from "./material.module";
 
-import { PageStructureModule } from './page-structure/page-structure.module';
-import { AuthenticationService } from './services';
-
+import { PageStructureModule } from "./page-structure/page-structure.module";
+import { AuthenticationService } from "./services";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -22,8 +20,9 @@ import { AuthenticationService } from './services';
     HttpClientModule,
     DemoMaterialModule,
     PageStructureModule,
+    NgbModule
   ],
   providers: [AuthenticationService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
