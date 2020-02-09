73133465c5b4577cefe6d4ffd56d1220b8afbf92
Sergio David Romero Maldonado <sromerom@psl.com.co>
2020-02-08 21:38:39 -0500

    Adiciono primera versión de formulario de login

--- html5/src/app/page-structure/home/home.component.ts
+++ html5/src/app/page-structure/home/home.component.ts
@@ -1,50 +1,59 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { first } from "rxjs/operators";
+import { AuthenticationService } from "../../services/authentication.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html'
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
-  chatForm: FormGroup;
+  title: string = "Olímpicos Tokyo 2020";
+  loginForm: FormGroup;
   loading = false;
   submitted = false;
-  returnUrl: string;
-
+  returnUrl: string = "/autenticado";
 
   constructor(
-    private formBuilder: FormBuilder
-  ) { }
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {}
 
   ngOnInit() {
-    this.chatForm = this.formBuilder.group({
-      q: ['', Validators.required]
+    this.loginForm = this.formBuilder.group({
+      username: ["", Validators.required],
+      password: ["", Validators.required]
     });
   }
 
-  get f() { return this.chatForm.controls; }
-
-  onSubmit() {
+  login() {
     this.submitted = true;
-
     // stop here if form is invalid
-    if (this.chatForm.invalid) {
+    if (this.loginForm.invalid) {
       return;
     }
 
-    console.log(this.f.q)
+    //console.log(this.f.q);
 
-    // this.loading = true;
-    // this.authenticationService.login(this.f.email.value, this.f.password.value)
-    //   .pipe(first())
-    //   .subscribe(
-    //     data => {
-    //       this.router.navigate([this.returnUrl]);
-    //     },
-    //     error => {
-    //       // this.alertService.error(error);
-    //       this.loading = false;
-    //     });
+    let { username, password } = this.loginForm.value;
+    console.log('username', username);
+    console.log('password', password);
+
+    this.loading = true;
+    this.authenticationService
+      .login(username, password)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.router.navigate([this.returnUrl]);
+        },
+        error => {
+          //this.alertService.error(error);
+          this.loading = false;
+        }
+      );
   }
-
 }
