0dad643bd90dc1baeb34e977bb8d83acd4d8f351
Juan Martínez <juan.c.a.martinez@accenture.com>
2020-02-09 23:27:15 -0500

    .

--- html5/src/app/app-routing.module.ts
+++ html5/src/app/app-routing.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './page-structure/home/home.component';
-import { LoginComponent } from './modules/users/login/login.component';
-import { RegisterComponent } from './modules/users/register/register.component';
+// import { LoginComponent } from './modules/users/login/login.component';
+// import { RegisterComponent } from './modules/users/register/register.component';
+import { UsersModule } from './modules/users/users.module';
 
 
 const routes: Routes = [
-  // {
-  //   path: 'users',
-  //   loadChildren: './modules/users/users.module#UsersModule'
-  // },
+  {
+    path: 'users',
+    loadChildren: () => UsersModule
+  },
 
   // { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '', component: HomeComponent },
-  { path: 'users/login', component: LoginComponent },
-  { path: 'users/register', component: RegisterComponent }
+  // { path: 'users/login', component: LoginComponent },
+  // { path: 'users/register', component: RegisterComponent }
 ];
 
 @NgModule({
